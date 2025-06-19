7ba6050245e2a3fecda28351830cf0ef131a2e4a
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-19 15:26:13 +0630

    Ch9-Ep56-home page designs

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className='bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 h-48 flex justify-center items-center'>
+        <div className="text-center space-y-2">
+          <h1 className="text-white text-3xl">Welcome From My Library</h1>
+          <p className="text-gray-300 text-sm">place where you can store and manage your books.</p>
+        </div>
+      </div>
+  )
+}
