42918c9ae6d45b60ed299b399852df64ab4e9a17
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-20 22:02:09 +0630

    Ch9-Ep64-add book search feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
+
+  let [search,setSearch]=useState('');
+  let navigate=useNavigate();
+
+  let handleSearch=(e)=>{
+    navigate('/?search='+search);
+  }
+
   return (
     <nav className='border border-b-1'>
             <ul className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
@@ -9,7 +17,12 @@ export default function Navbar() {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                   </svg>
-                  <input type='text' placeholder='search books ...' className='outline-none'></input>
+                  <input value={search} onChange={e=>setSearch(e.target.value)} type='text' placeholder='search books ...' className='outline-none'></input>
+
+                  <button onClick={handleSearch} className='text-white bg-indigo-600 px-3 py-1 rounded-2xl flex items-center gap-1'>
+                      <span className='hidden md:block'>Search</span>
+                    </button>
+
                 </li>
 
                 <Link to="/" className='flex items-center gap-3 md:-ml-32 cursor-pointer'>
