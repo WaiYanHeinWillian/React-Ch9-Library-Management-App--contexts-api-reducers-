ff5bab6b06b6dd858ff342ea4f111826bed8d183
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-20 15:28:41 +0630

    Ch9-Ep62-add categories feature

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -5,45 +5,55 @@ export default function Create() {
   let [title,setTitle]=useState('');
   let [description,setDescription]=useState('');
   let [newCategory,setNewCategory]=useState('');
+  let [categories,setCategories]=useState([]);
+
+  let addCategory=()=>{
+    setCategories(prev => [newCategory,...prev]);
+    setNewCategory('');
+  }
 
 
   return (
-    <form class="w-full max-w-lg mx-auto mt-5">
+    <form className="w-full max-w-lg mx-auto mt-5">
       
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
             Book Title
           </label>
-          <input value={title} onChange={e=>setTitle(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Book Title"/>
+          <input value={title} onChange={e=>setTitle(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Book Title"/>
         </div>
       </div>
 
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
             Book Description
           </label>
-          <textarea value={description} onChange={e=>setDescription(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Book Description"/>
+          <textarea value={description} onChange={e=>setDescription(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Book Description"/>
         </div>
       </div>
 
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
             Categories
           </label>
           
           <div className='flex items-center space-x-2'>
-            <input value={newCategory} onChange={e=>setNewCategory(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Book Category"/>
+            <input value={newCategory} onChange={e=>setNewCategory(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Book Category"/>
 
-            <button className='bg-indigo-600 p-1 rounded-lg mb-3'>
+            <button type='button' onClick={addCategory} className='bg-indigo-600 p-1 rounded-lg mb-3'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 p-1 text-white">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
               </svg>
             </button>
           </div>
-
+        </div>
+        <div className="flex flex-wrap">
+                    {categories.map(c=>(
+                      <span key={c} className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-indigo-600">{c}</span>
+                    ))}
         </div>
       </div>
 
