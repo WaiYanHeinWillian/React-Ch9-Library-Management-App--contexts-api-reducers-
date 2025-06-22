f7c78cd10bb2f26e4876ca4f47ec7f5039201178
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-22 09:59:32 +0630

    Ch9-Ep-72-minor theme switcher bug fix

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
@@ -10,6 +10,18 @@ export default function Layout() {
   const location=useLocation();
 
   let {isDark}=useTheme();
+
+  useEffect(()=>{
+    let body=document.body;
+
+    if(isDark){
+      //body class='bg-gray-950'
+      body.classList.add("bg-gray-950")
+    }else{
+      //body class = ""
+      body.classList.remove("bg-gray-950")
+    }
+  })
 
   return (
     <div className={isDark ? 'bg-gray-950' : 'bg-white'}>
