f08bd699be46af2739ac905fcff3bdb37c395286
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-26 11:46:28 +0630

    Ch11-Ep82-signup with firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function Login() {
   return (
-        <div class="w-full max-w-lg mt-16 mx-auto">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="w-full max-w-lg mt-16 mx-auto">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <h1 className='text-2xl font-bold text-indigo-600 my-5'>Login Form</h1>
-                <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                     Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="email"/>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="email"/>
                 </div>
-                <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                     Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
                 </div>
                 <div class="flex items-center justify-between">
                 <button class="bg-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
