78b93b74d21a6e2fcb6f059198c0ed76adf1255d
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-26 14:49:19 +0630

    Ch11-Ep83-signin with firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import useSignin from '../hooks/useSignIn';
 
 export default function Login() {
+
+    let [email,setEmail]=useState('');
+    let [password,setPassword]=useState('');
+    let navigate=useNavigate();
+
+    let {error,loading,signIn}=useSignin();
+
+    let login=async(e)=>{
+        e.preventDefault();
+        let user=await signIn(email,password);
+        
+        navigate('/')
+
+    }
+
   return (
         <div className="w-full max-w-lg mt-16 mx-auto">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={login}>
                 <h1 className='text-2xl font-bold text-indigo-600 my-5'>Login Form</h1>
                 <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                     Email
                 </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="email"/>
+                <input value={email} onChange={e=>setEmail(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="email"/>
                 </div>
                 <div className="mb-6">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                     Password
                 </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
+                <input value={password} onChange={e=>setPassword(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                {error && <p className="text-red-500 text-xs italic">{error}</p>}
                 </div>
-                <div class="flex items-center justify-between">
-                <button class="bg-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <div className="flex items-center justify-between">
+                <button className="flex items-center bg-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    {loading && <svg className="mr-3 -ml-1 size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>}
                     Login
                 </button>
                 
                 </div>
             </form>
-    <p class="text-center text-gray-500 text-xs">
+    <p className="text-center text-gray-500 text-xs">
         &copy;2020 Acme Corp. All rights reserved.
     </p>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ export default function Register() {
 
   let [email,setEmail]=useState('');
   let [password,setPassword]=useState('');
-  let navigate=useNavigate()
+  let navigate=useNavigate();
 
   let registerUser=async(e)=>{
     e.preventDefault();
@@ -36,7 +36,7 @@ export default function Register() {
             </div>
             <div className="flex items-center justify-between">
             <button className="flex items-center bg-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-              {loading && <svg className="mr-3 -ml-1 size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>}
+              {loading && <svg className="mr-3 -ml-1 size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>}
                 Register
             </button>
             
